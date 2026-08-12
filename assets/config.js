0ad7aa5d0c22da4271e4eecf42ef108b596aeb1d
als7s005 <chussein126@gmail.com>
2026-08-12 05:29:30 +0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,2 @@
-/*
-  بيانات مشروع Supabase
-  لا تضع SERVICE_ROLE KEY هنا أبدًا.
-*/
 window.SUPABASE_URL = "https://gurmxkqozlzpqwuwcpdc.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_r3UUemcradDnehrl0tCGgw_QcoN9-Pq";
